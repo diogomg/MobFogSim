762cc6643bd4be1dbc16be00fbfb400896a6a351
Diogo Goncalves <diogomachadogoncalves@gmail.com>
2020-01-28 14:19:45 -0300

    cleaning up the cloudbus.cloudsim.power.lists package

--- src/org/cloudbus/cloudsim/power/lists/PowerVmList.java
+++ src/org/cloudbus/cloudsim/power/lists/PowerVmList.java
@@ -1,9 +1,8 @@
 /*
- * Title:        CloudSim Toolkit
- * Description:  CloudSim (Cloud Simulation) Toolkit for Modeling and Simulation of Clouds
- * Licence:      GPL - http://www.gnu.org/copyleft/gpl.html
- *
- * Copyright (c) 2009-2012, The University of Melbourne, Australia
+ * Title: CloudSim Toolkit Description: CloudSim (Cloud Simulation) Toolkit for
+ * Modeling and Simulation of Clouds Licence: GPL -
+ * http://www.gnu.org/copyleft/gpl.html Copyright (c) 2009-2012, The University
+ * of Melbourne, Australia
  */
 
 package org.cloudbus.cloudsim.power.lists;
@@ -17,18 +16,16 @@ import org.cloudbus.cloudsim.core.CloudSim;
 import org.cloudbus.cloudsim.lists.VmList;
 
 /**
- * PowerVmList is a collection of operations on lists of power-enabled VMs.
- * 
- * If you are using any algorithms, policies or workload included in the power package, please cite
- * the following paper:
- * 
- * Anton Beloglazov, and Rajkumar Buyya, "Optimal Online Deterministic Algorithms and Adaptive
- * Heuristics for Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
- * Cloud Data Centers", Concurrency and Computation: Practice and Experience (CCPE), Volume 24,
- * Issue 13, Pages: 1397-1420, John Wiley & Sons, Ltd, New York, USA, 2012
+ * PowerVmList is a collection of operations on lists of power-enabled VMs. If
+ * you are using any algorithms, policies or workload included in the power
+ * package, please cite the following paper: Anton Beloglazov, and Rajkumar
+ * Buyya, "Optimal Online Deterministic Algorithms and Adaptive Heuristics for
+ * Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
+ * Cloud Data Centers", Concurrency and Computation: Practice and Experience
+ * (CCPE), Volume 24, Issue 13, Pages: 1397-1420, John Wiley & Sons, Ltd, New
+ * York, USA, 2012
  * 
  * @author Anton Beloglazov
- * 
  * @author Anton Beloglazov
  * @since CloudSim Toolkit 2.0
  */
@@ -37,7 +34,8 @@ public class PowerVmList extends VmList {
 	/**
 	 * Sort by cpu utilization.
 	 * 
-	 * @param vmList the vm list
+	 * @param vmList
+	 *        the vm list
 	 */
 	public static <T extends Vm> void sortByCpuUtilization(List<T> vmList) {
 		Collections.sort(vmList, new Comparator<T>() {
