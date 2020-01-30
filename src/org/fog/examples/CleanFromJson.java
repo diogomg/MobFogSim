f5a2d50500fadc66ec818d10b2d246aba7407221
Diogo Goncalves <diogomachadogoncalves@gmail.com>
2020-01-30 15:23:05 -0300

    cleaning up the fog.examples package

--- src/org/fog/examples/CleanFromJson.java
+++ src/org/fog/examples/CleanFromJson.java
@@ -18,10 +18,9 @@ import org.fog.placement.ModuleMapping;
 import org.fog.utils.JsonToTopology;
 
 /**
+ * Simulation setup for EEG Beam Tractor Game extracting physical topology
  * 
- * Simulation setup for EEG Beam Tractor Game extracting physical topology 
  * @author Harshit Gupta
- *
  */
 public class CleanFromJson {
 
@@ -38,25 +37,26 @@ public class CleanFromJson {
 			CloudSim.init(num_user, calendar, trace_flag);
 
 			String appId = "vr_game";
-			
+
 			FogBroker broker = new FogBroker("broker");
-			
+
 			Application application = createApplication(appId, broker.getId());
 			application.setUserId(broker.getId());
-			
+
 			/*
 			 * Creating the physical topology from specified JSON file
 			 */
-			PhysicalTopology physicalTopology = JsonToTopology.getPhysicalTopology(broker.getId(), appId, "topologies/routerTopology");
-						
-			ModuleMapping moduleMapping = ModuleMapping.createModuleMapping(); // create empty module to device mapping
-			
-			Controller controller = new Controller("master-controller", physicalTopology.getFogDevices(), physicalTopology.getSensors(), 
-					physicalTopology.getActuators(), moduleMapping);
-			
-			
+			PhysicalTopology physicalTopology = JsonToTopology.getPhysicalTopology(
+				broker.getId(), appId, "topologies/routerTopology");
+			// create empty module to device mapping
+			ModuleMapping moduleMapping = ModuleMapping.createModuleMapping();
+
+			Controller controller = new Controller("master-controller",
+				physicalTopology.getFogDevices(), physicalTopology.getSensors(),
+				physicalTopology.getActuators(), moduleMapping);
+
 			controller.submitApplication(application, 0);
-			
+
 			CloudSim.startSimulation();
 
 			CloudSim.stopSimulation();
@@ -67,37 +67,62 @@ public class CleanFromJson {
 			Log.printLine("Unwanted errors happen");
 		}
 	}
-	
-	
+
 	@SuppressWarnings({ "serial" })
-	private static Application createApplication(String appId, int userId){
-		
+	private static Application createApplication(String appId, int userId) {
+
 		Application application = Application.createApplication(appId, userId);
 		application.addAppModule("client", 10);
 		application.addAppModule("classifier", 10);
 		application.addAppModule("tuner", 10);
-		
+
 		application.addTupleMapping("client", "TEMP", "_SENSOR", new FractionalSelectivity(1.0));
-		application.addTupleMapping("client", "CLASSIFICATION", "ACTUATOR", new FractionalSelectivity(1.0));
-		application.addTupleMapping("classifier", "_SENSOR", "CLASSIFICATION", new FractionalSelectivity(1.0));
-		application.addTupleMapping("classifier", "_SENSOR", "HISTORY", new FractionalSelectivity(0.1));
-		application.addTupleMapping("tuner", "HISTORY", "TUNING_PARAMS", new FractionalSelectivity(1.0));
-	
+		application.addTupleMapping("client", "CLASSIFICATION", "ACTUATOR",
+			new FractionalSelectivity(1.0));
+		application.addTupleMapping("classifier", "_SENSOR", "CLASSIFICATION",
+			new FractionalSelectivity(1.0));
+		application.addTupleMapping("classifier", "_SENSOR", "HISTORY",
+			new FractionalSelectivity(0.1));
+		application.addTupleMapping("tuner", "HISTORY", "TUNING_PARAMS",
+			new FractionalSelectivity(1.0));
+
 		application.addAppEdge("TEMP", "client", 1000, 100, "TEMP", Tuple.UP, AppEdge.SENSOR);
-		application.addAppEdge("client", "classifier", 8000, 100, "_SENSOR", Tuple.UP, AppEdge.MODULE);
-		application.addAppEdge("classifier", "tuner", 1000000, 100, "HISTORY", Tuple.UP, AppEdge.MODULE);
-		application.addAppEdge("classifier", "client", 1000, 100, "CLASSIFICATION", Tuple.DOWN, AppEdge.MODULE);
-		application.addAppEdge("tuner", "classifier", 1000, 100, "TUNING_PARAMS", Tuple.DOWN, AppEdge.MODULE);
-		application.addAppEdge("client", "MOTOR", 1000, 100, "ACTUATOR", Tuple.DOWN, AppEdge.ACTUATOR);
-		
-		
-		final AppLoop loop1 = new AppLoop(new ArrayList<String>(){{add("TEMP");add("client");add("classifier");add("client");add("MOTOR");}});
-		final AppLoop loop2 = new AppLoop(new ArrayList<String>(){{add("classifier");add("tuner");add("classifier");}});
-		List<AppLoop> loops = new ArrayList<AppLoop>(){{add(loop1);add(loop2);}};
-		
+		application.addAppEdge("client", "classifier", 8000, 100, "_SENSOR", Tuple.UP,
+			AppEdge.MODULE);
+		application.addAppEdge("classifier", "tuner", 1000000, 100, "HISTORY", Tuple.UP,
+			AppEdge.MODULE);
+		application.addAppEdge("classifier", "client", 1000, 100, "CLASSIFICATION", Tuple.DOWN,
+			AppEdge.MODULE);
+		application.addAppEdge("tuner", "classifier", 1000, 100, "TUNING_PARAMS", Tuple.DOWN,
+			AppEdge.MODULE);
+		application.addAppEdge("client", "MOTOR", 1000, 100, "ACTUATOR", Tuple.DOWN,
+			AppEdge.ACTUATOR);
+
+		final AppLoop loop1 = new AppLoop(new ArrayList<String>() {
+			{
+				add("TEMP");
+				add("client");
+				add("classifier");
+				add("client");
+				add("MOTOR");
+			}
+		});
+		final AppLoop loop2 = new AppLoop(new ArrayList<String>() {
+			{
+				add("classifier");
+				add("tuner");
+				add("classifier");
+			}
+		});
+		List<AppLoop> loops = new ArrayList<AppLoop>() {
+			{
+				add(loop1);
+				add(loop2);
+			}
+		};
+
 		application.setLoops(loops);
-		
-		//GeoCoverage geoCoverage = new GeoCoverage(-100, 100, -100, 100);
+
 		return application;
 	}
 }
