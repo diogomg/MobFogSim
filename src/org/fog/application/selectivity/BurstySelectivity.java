76257afb2a553b15d09128a75cc251ad2511f750
Diogo Goncalves <diogomachadogoncalves@gmail.com>
2020-01-30 13:31:49 -0300

    cleaning up the fog.application.selectivity package

--- src/org/fog/application/selectivity/BurstySelectivity.java
+++ src/org/fog/application/selectivity/BurstySelectivity.java
@@ -4,54 +4,57 @@ import org.cloudbus.cloudsim.core.CloudSim;
 
 /**
  * Generates an output tuple for every input tuple according to a bursty model.
- * During high burst period, all input tuples result in an output tuple.
- * During low burst period, no input tuples result in an output tuple.
+ * During high burst period, all input tuples result in an output tuple. During
+ * low burst period, no input tuples result in an output tuple.
+ * 
  * @author Harshit Gupta
- *
  */
-public class BurstySelectivity implements SelectivityModel{
+public class BurstySelectivity implements SelectivityModel {
 
 	/**
 	 * Duration of the low burst period
 	 */
 	double burstLowPeriod;
-	
+
 	/**
 	 * Duration of the high burst period
 	 */
 	double burstHighPeriod;
-	
+
 	/**
-	 * First instance of the start of high burst period, using which subsequent burst periods will be calculated.
+	 * First instance of the start of high burst period, using which subsequent
+	 * burst periods will be calculated.
 	 */
 	double firstHighTime;
-	
-	public BurstySelectivity(double burstLowPeriod, double burstHighPeriod, double firstHighTime){
+
+	public BurstySelectivity(double burstLowPeriod, double burstHighPeriod, double firstHighTime) {
 		setBurstLowPeriod(burstLowPeriod);
 		setBurstHighPeriod(burstHighPeriod);
 		setFirstHighTime(firstHighTime);
 	}
-	
+
 	/**
-	 * If the current time falls in the high burst period of the specified burst model, an output tuple is generated for the incoming tuple.
+	 * If the current time falls in the high burst period of the specified burst
+	 * model, an output tuple is generated for the incoming tuple.
 	 */
 	@Override
 	public boolean canSelect() {
 		double time = CloudSim.clock() + getFirstHighTime();
-		double burstPeriod = getBurstHighPeriod()+getBurstLowPeriod();
-		double burstStartTime = burstPeriod*((int)(time/burstPeriod));
-		if(time <= burstStartTime + getBurstHighPeriod())
+		double burstPeriod = getBurstHighPeriod() + getBurstLowPeriod();
+		double burstStartTime = burstPeriod * ((int) (time / burstPeriod));
+		if (time <= burstStartTime + getBurstHighPeriod())
 			return true;
 		else
 			return false;
 	}
 
 	/**
-	 * The mean tuple generation rate is the fraction of high burst period in the total burst period.
+	 * The mean tuple generation rate is the fraction of high burst period in
+	 * the total burst period.
 	 */
 	@Override
 	public double getMeanRate() {
-		return getBurstHighPeriod()/(getBurstHighPeriod()+getBurstLowPeriod());
+		return getBurstHighPeriod() / (getBurstHighPeriod() + getBurstLowPeriod());
 	}
 
 	/**
--- src/org/fog/application/selectivity/FractionalSelectivity.java
+++ src/org/fog/application/selectivity/FractionalSelectivity.java
@@ -1,42 +1,77 @@
 package org.fog.application.selectivity;
 
 /**
- * Generates an output tuple for an incoming input tuple with a fixed probability
+ * Generates an output tuple for an incoming input tuple with a fixed
+ * probability
+ * 
  * @author Harshit Gupta
- *
  */
-public class FractionalSelectivity implements SelectivityModel{
+public class FractionalSelectivity implements SelectivityModel {
 
 	/**
 	 * The fixed probability of output tuple creation per incoming input tuple
 	 */
 	double selectivity;
-	
-	public FractionalSelectivity(double selectivity){
+
+	/**
+	 * Defines the selectivity value
+	 * 
+	 * @param selectivity
+	 */
+	public FractionalSelectivity(double selectivity) {
 		setSelectivity(selectivity);
 	}
+
+	/**
+	 * Gets the selectivity.
+	 * 
+	 * @return selectivity value
+	 */
 	public double getSelectivity() {
 		return selectivity;
 	}
+
+	/**
+	 * Sets the selectivity.
+	 * 
+	 * @param selectivity
+	 *        value
+	 */
 	public void setSelectivity(double selectivity) {
 		this.selectivity = selectivity;
 	}
-	
+
+	/**
+	 * Randomly defines if the tuple will be select based its selectivity rate
+	 * 
+	 * @return true in case the random value
+	 */
 	@Override
 	public boolean canSelect() {
-		if(Math.random() < getSelectivity()) // if the probability condition is satisfied
+		if (Math.random() < getSelectivity()) // if the probability condition is satisfied
 			return true;
 		return false;
 	}
-	
+
+	/**
+	 * Gets the average rate of tuple generation which is fixed by the
+	 * selectivity value
+	 * 
+	 * @return selectivity value
+	 */
 	@Override
 	public double getMeanRate() {
-		return getSelectivity(); // the average rate of tuple generation is the fixed probability value
+		return getSelectivity();
 	}
-	
+
+	/**
+	 * Gets the maximum rate of tuple generation which is fixed by the
+	 * selectivity value
+	 * 
+	 * @return selectivity value
+	 */
 	@Override
 	public double getMaxRate() {
-		return getSelectivity(); // the maximum rate of tuple generation is the fixed probability value
+		return getSelectivity();
 	}
-	
 }
